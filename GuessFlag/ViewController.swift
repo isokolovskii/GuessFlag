24b4ffd891c2b96e0a1e74e9538009eefe19cb18
Соколовский Иван Павлович <ivan3177@gmail.com>
2022-11-27 16:34:30 +0400

    Initial Commit

---
+++ GuessFlag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessFlag
+//
+//  Created by Иван Соколовский on 27.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
